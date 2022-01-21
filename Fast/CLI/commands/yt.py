8e389cbc0d993b41cfd402a9b256eaf39de659a2
Jack Soni <95261764+CoolAQ3D@users.noreply.github.com>
2022-01-21 02:12:15 +0000

    68 - rich table for download info yt

--- Fast/CLI/commands/yt.py
+++ Fast/CLI/commands/yt.py
@@ -1,9 +1,11 @@
 
 
-from rich.console import Console
-
 #Needed to Fix some lines of codes to start working pytube again, thats why pytube library won't work for now until its fixed and thats why needed to put the folder instead
 from Fast.pytube import YouTube
+
+
+from rich.console import Console
+from rich.table import Table
 console = Console()
 
 
@@ -25,6 +27,8 @@ def yt(url):
 
 
 def download_video(url):
+  if url == ".":
+    url = "https://www.youtube.com/watch?v=DAjMZ6fCPOo"
 
   settings = UserData.load()
 
@@ -34,7 +38,7 @@ def download_video(url):
   
   try: 
     url = str(url)
-    print(url)
+    #print(url)
     youtube = YouTube(url)
   except:
     return print('Invalid Video Link')
@@ -49,8 +53,42 @@ def download_video(url):
     video = youtube.streams.filter(only_audio=True).first()
   
   downloadFile = video.download(f"./{download_location}")
-  print(f'Downloading {url}')
+  
+  
+  video_name = youtube.title
+  video_size = convert_bytes(video.filesize)
+   
+  generate_rich_table(url, video_name, video_size, video_type, video_quality, download_location)
 
-  print(f'Video_Type: {video_type}')
-  print(f'Video_Quality: {video_quality}')
-  return print(downloadFile)
+
+def generate_rich_table(url, video_name, video_size, video_type, video_quality, download_location):
+  table = Table(show_header=True, header_style="bold red")
+  table.add_column("Category", style="#8EEA18", width=12)
+  table.add_column("YouTube Video Information", width=50)
+  table.add_row(
+    "Name", video_name
+  )
+  table.add_row(
+    "Size", video_size
+  )
+  table.add_row(
+    "Type", video_type
+  )
+  table.add_row(
+    "Quality", video_quality
+  )
+  table.add_row(
+    "Location", f"./{download_location}"
+  )
+  table.add_row(
+    "URL", url
+  )
+
+  console.print(table)
+
+def convert_bytes(size):
+      for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+          if size < 1024.0:
+              return "%3.1f %s" % (size, x)
+          size /= 1024.0   
+      return size
